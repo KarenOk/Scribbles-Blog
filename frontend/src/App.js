e3c8b930b9394eaef7710f52e30ccfe0840e9d92
karenok <karenokonkwo29@gmail.com>
2020-09-06 15:19:48 +0000

    Change loader to logo embedded in spin animation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./App.css";
+import { useAuth0 } from "@auth0/auth0-react";
 import Header from "./components/Header";
 import Posts from "./components/Posts";
 import Footer from "./components/Footer";
-import { useAuth0 } from "@auth0/auth0-react";
 import Banner from "./components/Banner";
+import logo from "./logo.png";
 
 function App() {
 	const { isAuthenticated, isLoading } = useAuth0();
@@ -12,7 +13,9 @@ function App() {
 	if (isLoading) {
 		return (
 			<div className="app d-flex align-items-center justify-content-center">
-				<h1>Loading ...</h1>
+				<div className="lds-dual-ring">
+					<img src={logo} alt="Loading..." />
+				</div>
 			</div>
 		);
 	}
