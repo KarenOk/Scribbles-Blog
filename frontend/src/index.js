7a5f2b60303a017bebc129c53902aabf95f2d2b7
karenok <karenokonkwo29@gmail.com>
2020-09-06 04:02:09 +0000

    Install auth0 react SDK

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,10 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.render(
 	<React.StrictMode>
-		<App />
+		<Auth0Provider
+			domain={process.env.REACT_APP_AUTH0_DOMAIN}
+			clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
+			redirectUri={window.location.origin}
+		>
+			<App />
+		</Auth0Provider>
 	</React.StrictMode>,
 	document.getElementById("root")
 );
