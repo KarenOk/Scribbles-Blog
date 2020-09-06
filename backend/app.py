1719b2c3662a601e1baa0d85a01963e595bf234a
karenok <karenokonkwo29@gmail.com>
2020-09-06 20:19:02 +0000

    Include error handlers for expected errors

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,6 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_cors import CORS
-from models import setup_db
+from models import setup_db, Post, Comment
 
 
 def create_app():
@@ -13,6 +13,48 @@ def create_app():
         return "Welcome to Scribbles API! We're lucky to have you."
 
     return app
+
+    # Expected errors - 400, 401, 403, 404, 405, 422, 500, Auth Error
+
+    @app.errorhandler(400)
+    def bad_request(error):
+        jsonify({
+            "success": False,
+            "error": 400,
+            "message": "Your request was not formatted properly."
+        })
+
+    @app.errorhandler(404)
+    def not_found(error):
+        return jsonify({
+            "success": False,
+            "error": 400,
+            "message": "The requested resource was not found."
+        })
+
+    @app.errorhandler(422)
+    def unprocessable(error):
+        return jsonify({
+            "success": False,
+            "error": 422,
+            "message": "The server understood your request but could not process it."
+        })
+
+    @app.errorhandler(405)
+    def method_not_allowed(self):
+        return jsonify({
+            "success": False,
+            "error": 405,
+            "message": "Method not allowed."
+        })
+
+    @app.errorhandler(500)
+    def internal_server_error(error):
+        return jsonify({
+            "success": False,
+            "error": 500,
+            "message": "An error occurred on our end."
+        })
 
 
 app = create_app()
