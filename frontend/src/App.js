9857fe958c15537ad39f04cc987b7cfef3b67da4
karenok <karenokonkwo29@gmail.com>
2020-09-06 02:20:33 +0000

    Implement footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
 import "./App.css";
 import Header from "./components/Header";
+import Posts from "./components/Posts";
+import Footer from "./components/Footer";
 
 function App() {
 	return (
 		<div className="app">
 			<div className="container">
 				<Header />
+				<Posts />
 			</div>
+			<Footer />
 		</div>
 	);
 }
---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+	return (
+		<footer>
+			Made with ❤️ by <a href="http://github.com/KarenOk"> Karen Okonkwo. </a>
+		</footer>
+	);
+};
+
+export default Footer;
