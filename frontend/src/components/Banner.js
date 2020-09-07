0a9f2f9ddb8763fbce0fa06b501850ba88dd01c8
karenok <karenokonkwo29@gmail.com>
2020-09-07 20:47:57 +0000

    Indicate user's role

--- frontend/src/components/Banner.js
+++ frontend/src/components/Banner.js
@@ -5,7 +5,20 @@ const Banner = () => {
 	const { user } = useAuth0();
 	return (
 		<div className="banner">
-			<p>Logged in as {user.name}</p>
+			<p>
+				Logged in as {user.name}.
+				{user["https://scribbles-blog.com/roles"][0] === "author" ? (
+					<span>
+						{" "}
+						You're an<span className="highlight"> author</span>.{" "}
+					</span>
+				) : (
+					<span>
+						{" "}
+						You're a <span className="highlight"> reader</span>.{" "}
+					</span>
+				)}
+			</p>
 		</div>
 	);
 };
