abf69d0fd18c22df6894ad5001bc0f366d4a44ee
karenok <karenokonkwo29@gmail.com>
2020-09-08 02:51:29 +0000

    Update base url with hosted API

--- frontend/src/BASE_URL.js
+++ frontend/src/BASE_URL.js
@@ -3,7 +3,7 @@ let BASE_URL;
 if (window.location.origin.includes("localhost")) {
 	BASE_URL = "http://localhost:5000";
 } else {
-	BASE_URL = "http://localhost:5000";
+	BASE_URL = "https://scribbles-blog-api.herokuapp.com/";
 }
 
 export { BASE_URL };
