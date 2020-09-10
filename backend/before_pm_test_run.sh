c69e73e4b269e4d4b9dbe1cab008f6ad38a02f04
karenok <karenokonkwo29@gmail.com>
2020-09-10 22:49:52 +0000

    Update script to insert a record in the db

--- backend/before_pm_test_run.sh
+++ backend/before_pm_test_run.sh
@@ -1,2 +1,8 @@
-dropdb scribbles_blog && createdb scribbles_blog
-python manage.py db upgrade
+# drop and recreate datebase
+dropdb scribbles_blog_test && createdb scribbles_blog_test
+
+# setup db with migration script
+python manage.py db upgrade 
+
+# Populate with a row of data
+psql -d scribbles_blog_test -c "INSERT INTO public.posts VALUES (1, 'New post', 'Post body', NOW(), NOW(), 'Karen Okonkwo', NULL );"
